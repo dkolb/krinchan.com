8cfe7aa736dc7001225470a6cd6e741d362d65bc
David Kolb <david.kolb@krinchan.com>
2013-09-22 12:22:14 -0400

    Restructured application in prep of adding on blogging capabilities to the site.  Postgres dependency added on.  Application setup broken out into configApp.js.  Next step: extract a lot of the anonymous functions into the routes.  Break up the routes.

--- app.js
+++ app.js
@@ -3,74 +3,22 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var configApp = require('./lib/configApp.js')
+  ;
 
-var app = express();
+var appObjects = configApp.setUp();
+var app = appObjects.app;
+var pg = appObjects.pg;
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Application URLs
-app.get('/', routes.index);
-app.get('/contact', routes.contact);
-app.post('/contact', routes.submitContact);
-
-//Used in dev and staging to test error handlers.
-if ('development' == app.get('env') || 'staging' == app.get('env')) {
-  app.get('/errorTest', routes.errorTest);
-}
-
-
-//Error Handling
-if ('development' == app.get('env')) {
-  app.use(express.logger());
-  app.use(express.errorHandler());
-};
-
-if ('staging' == app.get('env') || 'production' == app.get('env')) {
-  app.use(express.logger());
-  app.use(function (err, req, res, next) {
-    if (req.accepts('html')) {
-      res.status('500');
-      res.render(
-        'error',
-        {
-          title   : 'Server Error',
-          message : 'An error occured on the server.' +
-            '  The developer has been notified.'
-        }      );
-    } else if (req.accepts('json')) {
-      res.write(500, '{"error": "Server Error"}');
-    } else {
-      res.write(500, "An error occured in the server.");
-    }
-  });
-}
-
-//Not found
-app.use(function(req, res, next) {
-  res.status(404);
-  res.render(
-    'error',
-    {
-      title: 'Page Not Found',
-      message: 'Sorry, that URL was not found.  Please go back, or go ' +
-        'to the <a href="/">home page</a>.'
-    }
-  );
-});
-
-http.createServer(app).listen(app.get('port'), function(){
+var server = app.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+//Fast shutdown
+process.on('SIGTERM', function() {
+  server.close(function() {
+    console.log("Closing out remaining connections.");
+    pg.end();
+  });
+});
+
